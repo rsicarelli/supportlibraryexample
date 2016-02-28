9f677755b746fd570d8d8ccea13c449b857f1e63
rsicarelli <rodrigo.sicarelli@gmail.com>
2016-02-28 00:32:13 -0300

    Implement android icon on navigation view

---
+++ app/src/main/java/br/com/rsicarelli/supportlibraryexample/home/Fragment.java
@@ -0,0 +1,36 @@
+package br.com.rsicarelli.supportlibraryexample.home;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.rsicarelli.supportlibraryexample.R;
+import butterknife.ButterKnife;
+
+/**
+ * Created by rodrigosicarelli on 2/28/16.
+ */
+public class Fragment extends android.support.v4.app.Fragment {
+
+    public static Fragment newInstance() {
+        return new Fragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(root, getActivity());
+
+        initUi();
+
+        return root;
+    }
+
+    private void initUi() {
+
+    }
+}
--- app/src/main/java/br/com/rsicarelli/supportlibraryexample/home/HomeActivity.java
+++ app/src/main/java/br/com/rsicarelli/supportlibraryexample/home/HomeActivity.java
@@ -1,5 +1,8 @@
 package br.com.rsicarelli.supportlibraryexample.home;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
 import br.com.rsicarelli.supportlibraryexample.R;
 import br.com.rsicarelli.supportlibraryexample.presentation.NavigationDrawerActivity;
 
@@ -8,6 +11,15 @@ public class HomeActivity extends NavigationDrawerActivity {
     @Override
     public void initializeContentView() {
         setContentView(R.layout.activity_home);
+
+        initFragment(Fragment.newInstance());
+    }
+
+    private void initFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.container, fragment);
+        transaction.commit();
     }
 
     @Override
